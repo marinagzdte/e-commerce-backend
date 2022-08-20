0caf26d555561bdf651f46882aa058f089d37b92
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-08-19 23:27:15 -0300

    config

--- src/config.js
+++ src/config.js
@@ -7,6 +7,15 @@ export default {
         }
     },
     firebase: {
-        // json
+        type: "service_account",
+        project_id: "e-commerce-backend-c2284",
+        private_key_id: "95ea9a4c7361818478ac0affc9a666a0d8078f1e",
+        private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCUZCnLwYDyAqPn\nPnRCsA4qIbgQ4209tvA0b5V9LedYAA1coZWojwP7tlmS/6KsN9ioNnXQT01Sw3AG\nXQyHo8fn1PIN9pHsMaMhg+iaArheS4OLcCSNuRiTeSaKdMVRvWvcPUA4jzAtQXSy\nNTg8g+UU2aPFB91OTxjdG21fECtyQyvFyDuBjmVp1NqoacEmSv1LqUlpkJ7z43Tj\nP8NVjw058/h4QsdDAeAArEL28txFbeCbGapU0CXYEBYjMJSCRvsnJKdoJNODDq6f\nDAAYT6KbH653sNsIejA1Z521TxIpeXlMDNL+TX9IOjUROpD5+tSSILZ9NeEKexLp\ni8mlmRGZAgMBAAECgf8k527S6EzT4ymKrlLmQuKYQnUvE2Bs+LzETaaKezVjAqBQ\nE995tsJG12gaJs6jW0AxQAS3dFbGk9RIa4Zc4AksnsjpWr5E/2iSl+ZBEcFzB1Sv\nrjQTtZ9nDboytNezJxNWso+bgVYklc1v2cx36wAN2hKLQxQcEDp0GAjF8OoygCEa\nGmqRamOWvneU/2jHKL/1zdTdaQLsxja67LD8AsfKVckQJlwr3YBtFOuRrIvCuIwq\nyOXXbVhYIlJvY4xchrbdb7pSQ6/UbXM6O04hg85vfylq1XjyUbvi05yguuKXbrxp\ncndSawBox/T7nAHEPP1W/9/pxnlPQy34SyrHexECgYEAxOzas1sjNc7qTLpmp51/\n0qtaMjmaEPCA/jQt7ul5lVLzJJCGC1YJ5OGMAMS6/BcF6CxpmMYgwxZeSpZzZoah\nksDpSJWvGu9xHa1bUfevOIM/I6DpSftm0OkrXw0yd0YzTbNaRiZiHRYXKRvQtC6X\n9HVpTfcWAjFUnSU7PWyqN60CgYEAwOgTqEyfoYOYkfri95aXc90TnTWxWxL9YeQg\nN/ish3HubcfUf6kMD2UoP5RaQvUKP+tXWMGml/KzIw/pegtuJn3AW8ZtpW73lQMN\ns18BK0xFWECuizt73HY2OM7yVT0jWHPFzhtmu1nhPSi/rw1qjrWOVhxOkMTD/Yt1\nhpF4Lx0CgYEAgPNkhtwgI2UaMiJBhHLzN92zE356r6r9nPhIN9E1J54Iz6G/1P6D\nPhif2AT7OIZO/OjQQRVEcNtd5VO9v55bgUDHwhsabfXrSXayfOtHFyeCme8NtL9m\nZVi6aOS4bjhO8guHCrapusgzZFztfn+gS5jy2vX4uHjbIog10ZQtpoUCgYBbXA4j\nQNbBKY/SYx2m4H0t9F/brpgmZ+H0wJO5rl4Xb4/aEi/Dng4GJIRhSCNPXrnR9+VQ\nNbZudBT+4ARVNqCePiQh0uIO3XUH74BHmRD2rpKQ72E6s5s/mASp9uiyEdhxhbMC\nn9uRk0mnK2tMvm/e0oFsqs9SpL2lkKzCZJakyQKBgQCEmr/CSZTTqLjUlQ+pQP1R\nfrZ+hXmaDWP9NPq6QzL7xOxwR693m7v7BPtOoQgFw8pK6yrAbhJHCt71pdyo+pUp\njb3lXerdiw3UbguoGLCkwqf7bg7QMM78aqclCuzZ5jf5fok85BWbs58EwVZdIYZ/\noKercQI90e2GpbXTp5kr8A==\n-----END PRIVATE KEY-----\n",
+        client_email: "firebase-adminsdk-3fauy@e-commerce-backend-c2284.iam.gserviceaccount.com",
+        client_id: "100020483297913054015",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3fauy%40e-commerce-backend-c2284.iam.gserviceaccount.com"
     }
 }
