8351a471474024e6313b40432875c3a18a200f5b
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-30 14:54:47 -0300

    add mongoose connect on server listen

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import app from './server.js';
+import logger from './utils/logger.js';
+import mongoose from 'mongoose';
+import config from './config.js';
 
 const PORT = Number(process.env.PORT) || 8080;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, async () => {
     console.log(`Servidor escuchando en puerto ${PORT}.`);
+    try {
+        await mongoose.connect(config.mongodb.connectionString, config.mongodb.options)
+        logger.logInfo('conectado a la db')
+    } catch (error) {
+        logger.logError(error)
+    }
 })
 
 server.on('error', error => console.log(`Error en servidor ${error}`))
