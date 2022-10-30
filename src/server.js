518998e1fabb46d09b4466dcc9709f847b730705
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-30 15:33:45 -0300

    add dependencies

--- src/server.js
+++ src/server.js
@@ -1,13 +1,47 @@
 import express from 'express';
-import productRouter from './routes/productRouter.js';
-import cartRouter from './routes/cartRouter.js';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from './middlewares/passport.js';
+import { engine } from 'express-handlebars';
+import compression from 'compression';
+import logger from './utils/logger.js';
+import { cartRouter, productRouter, sessionRouter } from "./routes/index.js";
 
 const app = express();
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000,
+        httpOnly: false,
+        secure: false
+    },
+    rolling: true
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: "./public/views/layouts",
+    partialsDir: "./public/views/partials"
+}));
+app.set('view engine', 'hbs');
+app.set('views', "./public/views");
+
+app.use(compression())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
 app.use('/api/productos', productRouter)
 app.use('/api/carrito', cartRouter)
+app.use('', sessionRouter);
 
 app.all('*', (req, res) => {
     res.status(405).json({ error: -2, descripcion: `ruta ${req.url} metodo ${req.method} no implementado` })
