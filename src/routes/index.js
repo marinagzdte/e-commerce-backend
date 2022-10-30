79d86ea76812f109ef30c8be4df10b066350585f
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-30 19:48:29 -0300

    routes

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import cartRouter from "./cartRouter.js";
+import productRouter from "./productRouter.js";
+import sessionRouter from "./sessionRouter.js";
+
+export { cartRouter, productRouter, sessionRouter }
