39780e2f73ed5bc3e622cf81b9f8204e870e2829
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-30 19:48:17 -0300

    strategies

---
+++ src/middlewares/passport.js
@@ -0,0 +1,80 @@
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import bCrypt from 'bcrypt';
+import logger from '../utils/logger.js';
+import { usersDao as usersApi } from '../daos/index.js'
+
+/*-----------------------------------------------*/
+/*                 passport                      */
+/*-----------------------------------------------*/
+
+const isValidPassword = (user, password) => {
+    return bCrypt.compareSync(password, user.password);
+}
+
+const hash = (password) => {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+}
+
+passport.serializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.use('login', new passportLocal.Strategy(
+    { passReqToCallback: true },
+    async (req, username, password, done) => {
+        try {
+            const user = await usersApi.getByCondition({ email: username });
+
+            if (!isValidPassword(user, password)) {
+                logger.logWarn('Contraseña inválida.');
+                return done(null, false);
+            }
+
+            logger.logInfo(`Usuario ${user.email} autenticado.`)
+            return done(null, user);
+        }
+        catch (error) {
+            logger.logWarn(`Usuario ${username} no encontrado.`);
+            return done(null, false);
+        }
+    }
+
+));
+
+passport.use('register', new passportLocal.Strategy({
+    passReqToCallback: true
+},
+    async (req, username, password, done) => {
+        try {
+            const user = await usersApi.getByCondition({ email: username });
+            if (user) {
+                logger.logWarn(`El usuario ${username} ya existe.`);
+                return done(null, false);
+            }
+            const newUser = {
+                email: username,
+                password: hash(password),
+                name: req.body.name,
+                age: req.body.age,
+                address: req.body.adress,
+                phoneNumber: req.body.phoneNumber,
+                avatar: req.body.avatar
+            }
+
+            await usersApi.save(newUser);
+            logger.logInfo(`Nuevo usuario registrado: ${newUser.email}`);
+            return done(null, newUser);
+
+        } catch (error) {
+            logger.logError(`error en registro: ${error}`)
+            return done(error);
+        }
+    }
+))
+
+export default passport;
