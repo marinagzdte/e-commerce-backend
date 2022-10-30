95b62317c057d7316fb67638a7784bd50109d503
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-30 19:47:18 -0300

    add fields

--- src/daos/MongoDbUsersDao.js
+++ src/daos/MongoDbUsersDao.js
@@ -5,7 +5,11 @@ class MongoDbUsersDao extends MongoDbContainer {
     constructor() {
         super('users', new mongoose.Schema({
             name: { type: String, required: true },
-            username: { type: String, required: true },
+            address: { type: String, required: true },
+            age: { type: Number, required: true },
+            phoneNumber: { type: String, required: true },
+            avatar: { type: String, required: true },
+            email: { type: String, required: true },
             password: { type: String, required: true }
         }));
     }
