564056732ac9093bed9dcc34d66c79c0af264692
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-13 14:53:11 -0300

    send email on signup

---
+++ src/utils/emailUtils.js
@@ -0,0 +1,40 @@
+import { sendMail } from "./nodeMailerUtils.js"
+
+const newMail = () => {
+    return {
+        from: 'e-commerce',
+        to: 'mgonzalezdante@gmail.com',
+        subject: null,
+        html: null
+    }
+}
+
+const sendNewUserEmail = async (user) => {
+    const options = newMail();
+    options.subject = 'Nuevo usuario registrado'
+    options.html = `<h1 style="color: blue;">Datos del usuario:</h1>
+                    <ul>
+                        <li>Nombre: ${user.name}</li>
+                        <li>Dirección: ${user.address}</li>
+                        <li>Edad: ${user.age}</li>
+                        <li>Número de teléfono: ${user.phoneNumber}</li>
+                        <li>Dirección de correo electrónico: ${user.email}</li>
+                    </ul>`
+    await sendMail(options)
+}
+
+const sendNewOrderEmail = async (order) => {
+    const options = newMail();
+    options.subject = 'Nuevo usuario registrado'
+    // options.html = `<h1 style="color: blue;">Datos del usuario:</h1>
+    //                 <ul>
+    //                     <li>Nombre: ${user.name}</li>
+    //                     <li>Dirección: ${user.address}</li>
+    //                     <li>Edad: ${user.age}</li>
+    //                     <li>Número de teléfono: ${user.phoneNumber}</li>
+    //                     <li>Dirección de correo electrónico: ${user.email}</li>
+    //                 </ul>`
+    await sendMail(options)
+}
+
+export { sendNewUserEmail, sendNewOrderEmail }
