e63bf24216436f34fc2bafc9c02588a1415178c7
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-13 14:52:57 -0300

    add nodemailer

---
+++ src/utils/nodeMailerUtils.js
@@ -0,0 +1,23 @@
+import { createTransport } from "nodemailer";
+import logger from "./logger";
+
+const transporter = createTransport({
+    service: 'gmail',
+    port: 587,
+    auth: {
+        user: 'mgonzalezdante@gmail.com',
+        pass: 'ykbyjxbbrhrjepib'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+export const sendMail = async (options) => {
+    try {
+        const info = await transporter.sendMail(options)
+        logger.logInfo(info)
+    } catch (error) {
+        logger.logError(error)
+    }
+}
