db4624d9967044ee88797584e1c90b8226d716f1
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-15 01:00:24 -0300

    add cart to user

--- src/daos/MongoDbUsersDao.js
+++ src/daos/MongoDbUsersDao.js
@@ -10,7 +10,8 @@ class MongoDbUsersDao extends MongoDbContainer {
             phoneNumber: { type: String, required: true },
             avatar: { type: String, required: true },
             email: { type: String, required: true },
-            password: { type: String, required: true }
+            password: { type: String, required: true },
+            cart: { type: String, required: true }
         }));
     }
 }
