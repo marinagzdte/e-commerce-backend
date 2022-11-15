a611450f7cfb7c5294945c1dabc6ab726229fad0
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-14 23:58:04 -0300

    new order email

--- src/utils/emailUtils.js
+++ src/utils/emailUtils.js
@@ -23,17 +23,26 @@ const sendNewUserEmail = async (user) => {
     await sendMail(options)
 }
 
+const writeProdList = (prods) => {
+    let list = "<ul>"
+    prods.forEach(p => {
+        list += `<li>${p.name} x ${p.amount} ${p.amount === 1 ? "unidad" : "unidades"} ($${p.price} c/u)</li>`
+    });
+    list += "</ul>"
+    return list
+}
+
 const sendNewOrderEmail = async (order) => {
     const options = newMail();
-    options.subject = 'Nuevo usuario registrado'
-    // options.html = `<h1 style="color: blue;">Datos del usuario:</h1>
-    //                 <ul>
-    //                     <li>Nombre: ${user.name}</li>
-    //                     <li>Dirección: ${user.address}</li>
-    //                     <li>Edad: ${user.age}</li>
-    //                     <li>Número de teléfono: ${user.phoneNumber}</li>
-    //                     <li>Dirección de correo electrónico: ${user.email}</li>
-    //                 </ul>`
+    options.subject = `Nuevo pedido del usuario ${order.user.name}`
+    options.html = `<h1 style="color: blue;">Detalle del pedido:</h1>
+                    ${writeProdList(order.products)}
+                    <h1 style="color: blue;">Datos del usuario:</h1>
+                    <ul>
+                        <li>Nombre: ${order.user.name}</li>
+                        <li>Número de teléfono: ${order.user.phoneNumber}</li>
+                        <li>Dirección de correo electrónico: ${order.user.email}</li>
+                    </ul>`
     await sendMail(options)
 }
 
