8581e3325b472e8b0d56ebffc670d0c3131eed1c
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-16 22:13:04 -0300

    daos index

--- src/daos/index.js
+++ src/daos/index.js
@@ -1,24 +1,17 @@
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
-
 let cartsDao;
 let productsDao;
+let usersDao;
 
 switch (process.env.PERSISTENCE) {
     case 'mongodb':
+    default:
         const { default: MongoDbProductsDao } = await import('./MongoDbProductsDao.js')
         const { default: MongoDbCartsDao } = await import('./MongoDbCartsDao.js');
+        const { default: MongoDbUsersDao } = await import('./MongoDbUsersDao.js');
         productsDao = new MongoDbProductsDao();
         cartsDao = new MongoDbCartsDao();
-        break;
-    case 'firebase':
-        const { default: FirebaseProductsDao } = await import('./FirebaseProductsDao.js')
-        const { default: FirebaseCartsDao } = await import('./FirebaseCartsDao.js');
-        productsDao = new FirebaseProductsDao();
-        cartsDao = new FirebaseCartsDao();
-        break;
-    default:
+        usersDao = new MongoDbUsersDao();
         break;
 }
 
-export { productsDao, cartsDao }
+export { productsDao, cartsDao, usersDao }
